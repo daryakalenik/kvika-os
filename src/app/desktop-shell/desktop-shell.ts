502078ad9849a7a47408c471620ca0d2054a6d78
Darya Kalenik <kalenikdarya@gmail.com>
2026-06-14 01:14:23 +0300

    feat: add global styles reset & top-bar basic styling

--- src/app/desktop-shell/desktop-shell.ts
+++ src/app/desktop-shell/desktop-shell.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TopBar } from './top-bar/top-bar';
+import { Workspace } from './workspace/workspace';
+import { BottomBar } from './bottom-bar/bottom-bar';
 
 @Component({
   selector: 'app-desktop-shell',
-  imports: [],
+  imports: [TopBar, Workspace, BottomBar],
   templateUrl: './desktop-shell.html',
   styleUrl: './desktop-shell.scss',
 })
